4c8c6310f72b41f6cfd50ce6884f9c16c7be5ea3
Divyanshu Avasthi <divya.14.avasthi@gmail.com>
2024-05-07 07:23:54 +0530

    updated the remote backend

--- EKS_Terraform_Jenkins/backend.tf
+++ EKS_Terraform_Jenkins/backend.tf
@@ -1,3 +1,4 @@
+// create a bucket in aws account inorder to use remote backend. It will be used to maintain the terraform state files
 terraform {
   backend "s3" {
     bucket = "divyanshu-avasthi-polaris-assignment-bucket"
